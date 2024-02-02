58fa543f1addf161c5200dd164fde03c1a9df786
Emmanuelle BONOLI <manu.bonoli@gmail.com>
2024-02-02 14:46:23 +0100

    début Schedule coworker

--- frontend/src/components/MenuUser.jsx
+++ frontend/src/components/MenuUser.jsx
@@ -1,5 +1,5 @@
 import { useContext, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 import PropTypes from "prop-types";
 import UserContext from "../contexts/UserContext";
 
@@ -9,10 +9,13 @@ function MenuUser({
   coworkers,
   listCoworkersAttente,
 }) {
+  const location = useLocation();
   const { userConnected, setUserConnected } = useContext(UserContext);
   const navigate = useNavigate();
   useEffect(() => {
-    coworkers();
+    if (location.pathname === "/planning") {
+      coworkers();
+    }
   }, []);
 
   const handlelogout = () => {
@@ -20,42 +23,57 @@ function MenuUser({
     localStorage.removeItem("token");
     navigate("/");
   };
-
-  return (
-    <div className="menuUser">
-      <div className="menu">
-        <h1>{userConnected.firstName}</h1>
-        <div className="detailsCoworker">
-          <div className="notifContainer">
-            <h2
+  if (location.pathname === "/planning") {
+    return (
+      <div className="menuUser">
+        <div className="menu">
+          <h1>{userConnected.firstName}</h1>
+          <div className="detailsCoworker">
+            <div className="notifContainer">
+              <h2
+                className="buttonCoworker"
+                role="presentation"
+                onClick={() => setOpenAllCoworkers(true)}
+              >
+                Co-Workers
+              </h2>
+              {listCoworkersAttente.length > 0 ? (
+                <p className="notif">{listCoworkersAttente.length}</p>
+              ) : (
+                ""
+              )}
+            </div>
+            <p
               className="buttonCoworker"
               role="presentation"
-              onClick={() => setOpenAllCoworkers(true)}
+              onClick={() => setOpenNewCoworker(true)}
             >
-              Co-Workers
-            </h2>
-            {listCoworkersAttente.length > 0 ? (
-              <p className="notif">{listCoworkersAttente.length}</p>
-            ) : (
-              ""
-            )}
-          </div>
-          <p
-            className="buttonCoworker"
-            role="presentation"
-            onClick={() => setOpenNewCoworker(true)}
-          >
-            Ajouter des Co-Workers
-          </p>
-          <div className="logoutContainer">
-            <button onClick={handlelogout} type="button" className="logout">
-              Se déconnecter
-            </button>
+              Ajouter des Co-Workers
+            </p>
+            <div className="logoutContainer">
+              <button onClick={handlelogout} type="button" className="logout">
+                Se déconnecter
+              </button>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
+  // if (location.pathname !== "/planning") {
+  //   <div className="menuUser">
+  //     <div className="menu">
+  //       <h1>Agenda de : {userConnected.firstName}</h1>
+  //       <div className="detailsCoworker">
+  //         <div className="logoutContainer">
+  //           <button type="button" className="logout">
+  //             Retour à mon agenda
+  //           </button>
+  //         </div>
+  //       </div>
+  //     </div>
+  //   </div>;
+  // }
 }
 
 MenuUser.propTypes = {
